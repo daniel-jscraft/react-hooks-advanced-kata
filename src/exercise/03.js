e64c3c3863c2783410d7c0258699a9d9e40567d5
Daniel <daniel.nastas@gmail.com>
2022-08-23 12:37:02 +0300

    custom hook for context

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,6 +3,14 @@ import {useState, useContext, createContext} from 'react'
 
 const CountContext = createContext()
 
+function useCount() {
+  const countContext = useContext(CountContext)
+  if (!countContext) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return countContext
+}
+
 function CountProvider(props) {
   const [x, setX] = useState(10)
   // use {...props} to render the children
@@ -10,12 +18,12 @@ function CountProvider(props) {
 }
 
 function CountDisplay() {
-  const [x] = useContext(CountContext)
+  const [x] = useCount()
   return <div>{`The current count is ${x}`}</div>
 }
 
 function Counter() {
-  const [ , setX] = useContext(CountContext)
+  const [ , setX] = useCount()
   const increment = () => setX(x => x + 1)
   return <button onClick={increment}>Increment count</button>
 }
